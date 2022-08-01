57cb0fb8b975072dc91e3ee918cd3c20919fcf32
Rohini Rao <64858610+RohiniRG@users.noreply.github.com>
2022-08-01 11:20:31 +0530

    Create unique_paths.py

---
+++ Daily Challenge/unique_paths.py
@@ -0,0 +1,27 @@
+# Day 1
+
+class Solution:
+    def uniquePaths(self, m: int, n: int, memo=dict()) -> int:
+        key_1 = f'{m}, {n}'
+        key_2 = f'{n}, {m}'
+        
+        if key_1 in memo:
+            return memo[key_1]
+        if key_2 in memo:
+            return memo[key_2]
+
+        if m == 1 and n == 1:
+            return 1
+        if m == 0 or n == 0:
+            return 0
+        
+        total = self.uniquePaths(m-1, n, memo) + self.uniquePaths(m, n-1, memo)
+        memo[key_1] = total
+        memo[key_2] = total
+        
+        return memo[key_1]
+        
+# RESULTS:
+# Runtime: 29 ms, faster than 97.03% of Python3 online submissions for Unique Paths.
+# Memory Usage: 14 MB, less than 32.97% of Python3 online submissions for Unique Paths.
+
